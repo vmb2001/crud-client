ad9243d79ce0dbc8bb0798ba31f2d18751fd3cf2
Varsha <varsha.biju@people10.com>
2023-09-25 13:24:41 +0530

    Commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,8 +6,7 @@ import axios from "axios";
 import Page from "./Page";
 
 function Table({ Data, setData }) {
-  //Function for sorting array
-
+  // Function for sorting array
   const Sort = (newdata) => {
     const ndata = newdata.sort((a, b) => {
       let fa = a.first_name.toLowerCase();
@@ -23,7 +22,7 @@ function Table({ Data, setData }) {
     return ndata;
   };
 
-  //Function to get current date
+  // Function to get current date
   const getDate = () => {
     const now = new Date();
     const date =
@@ -43,25 +42,19 @@ function Table({ Data, setData }) {
   const cdata = Sort([...Data]);
   const [searchValue, setSearchvalue] = useState("");
 
-  //For maintaining a copy of Data to reset table when search from is empty
-  const [updateData, setUpdatedata] = useState("");
-  useEffect(() => {
-    setUpdatedata(Data);
-  }, [Data]);
-
-  //Declaring variables required for pagination
+  // Declaring variables required for pagination
   const [currentPage, setcurrentPage] = useState(1);
-  const recordPerPage = 9;
+  const recordPerPage = 7;
   const lastIndex = currentPage * recordPerPage;
   const firstIndex = lastIndex - recordPerPage;
   const records = cdata.slice(firstIndex, lastIndex);
   const npage = Math.ceil(Data.length / recordPerPage);
 
-  //Function for adding customer
+  // Function for adding a customer
   const add = () => {
     Swal.fire({
       title: "Add Employee",
-      html: `<div className="form-group"><label for="swal-input1"  style="width: 100px;">Email ID:</label>
+      html: `<div className="form-group"><label for="swal-input1" style="width: 100px;">Email ID:</label>
           <input label="email" id="swal-input1" value="" class="swal2-input"><div/>
           <div className="form-group"><label for="swal-input2" style="width: 100px;">First Name:</label>
           <input id="swal-input2" value="" class="swal2-input"><div/>
@@ -73,7 +66,6 @@ function Table({ Data, setData }) {
           <input id="swal-input5" value="" class="swal2-input"><div/>
           <div className="form-group"><label for="swal-input6" style="width: 100px;">Longitude:</label>
           <input id="swal-input6" value="" class="swal2-input"><div/>`,
-
       showCancelButton: true,
       confirmButtonText: "Add",
     }).then((result) => {
@@ -85,59 +77,59 @@ function Table({ Data, setData }) {
         const new_latitude = document.getElementById("swal-input5").value;
         const new_longitude = document.getElementById("swal-input6").value;
 
-        let update = [...Data];
-        //Calculating id of the element to be added
-        let items = [];
-        update.map((item) => items.push(item.id));
-        const m = Math.max(...items);
-        const new_id = m + 1;
-
-        const date = getDate();
-
-        const obj = {
-          id: new_id,
+        // Create a new customer object
+        const newCustomer = {
           email: new_email,
           first_name: new_first_name,
           last_name: new_last_name,
           ip: new_ip,
           latitude: new_latitude,
           longitude: new_longitude,
-          created_at: date,
+          created_at: getDate(),
         };
-        update.push(obj);
-        update = Sort(update);
+
+        // Make an API call to add the new customer
         axios
-          .post("http://localhost:3001/api/customers", {
-            email: new_email,
-            first_name: new_first_name,
-            last_name: new_last_name,
-            ip: new_ip,
-            latitude: new_latitude,
-            longitude: new_longitude,
-            created_at: date,
+          .post("http://localhost:3001/api/customers", newCustomer)
+          .then((response) => {
+            if (response.status === 200) {
+              // The customer added successfully, so fetch updated data
+              axios
+                .get("http://localhost:3001/api")
+                .then((result) => {
+                  setData(Sort(result.data));
+                })
+                .catch((error) => {
+                  console.error("Error fetching data:", error);
+                });
+            }
           })
           .catch((err) => {
-            alert(`An error has occured while adding ${err}`);
+            alert(`An error has occurred while adding ${err}`);
           });
-        setUpdatedata(update);
-        setData(update);
       }
     });
   };
 
-  //Function to get value entered in the search form
+  // Function to get value entered in the search form
   const getSearchvalue = (e) => {
     const s = e.target.value;
     if (s === "") {
-      console.log("inside empty search");
-      //console.log(updateData);
-      setData(updateData);
+      //Reset data if search empty
+      axios
+        .get("http://localhost:3001/api")
+        .then((result) => {
+          setData(result.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+
       setSearchvalue("");
-      console.log("Searchvalue:" + searchValue);
     } else setSearchvalue(s);
   };
 
-  //Function to display search result
+  // Function to display search result
   const handleSearch = () => {
     let flag = 0;
     if (searchValue !== "") {
@@ -149,12 +141,12 @@ function Table({ Data, setData }) {
           return true;
         } else return false;
       });
-      if (flag === 0) new Swal("Not Found");
+      if (flag === 0) swal("Not Found");
       else setData(searchData);
     }
   };
 
-  //Function to delete customer
+  // Function to delete customer
   const del = (id) => {
     swal({
       title: "Are you sure?",
@@ -164,29 +156,36 @@ function Table({ Data, setData }) {
       dangerMode: true,
     }).then((willDelete) => {
       if (willDelete) {
-        const update = Data.filter((item) => item.id != id);
+        // Make an API call to delete the customer
         axios
           .delete(`http://localhost:3001/api/customers${id}`)
+          .then((response) => {
+            if (response.status === 200) {
+              // The customer was deleted successfully, so fetch updated data
+              axios
+                .get("http://localhost:3001/api")
+                .then((result) => {
+                  setData(Sort(result.data));
+                })
+                .catch((error) => {
+                  console.error("Error fetching data:", error);
+                });
+            }
+          })
           .catch((err) => {
-            alert(`An error has occured while deleting ${err}`);
+            alert(`An error has occurred while deleting ${err}`);
           });
-        setUpdatedata(update);
-        setData(update);
-        swal({
-          title: "Successfully Deleted",
-          icon: "success",
-        });
       }
     });
   };
 
-  //Function to edit customer details
+  // Function to edit customer details
   const update = (id) => {
     Data.map((item) => {
       if (item.id === id) {
         Swal.fire({
           title: "Edit Employee Details",
-          html: `<div className="form-group"><label for="swal-input1"  style="width: 100px;">Email ID:</label>
+          html: `<div className="form-group"><label for="swal-input1" style="width: 100px;">Email ID:</label>
                 <input label="email" id="swal-input1" value=${item.email} class="swal2-input"><div/>
                 <div className="form-group"><label for="swal-input2" style="width: 100px;">First Name:</label>
                 <input id="swal-input2" value=${item.first_name} class="swal2-input"><div/>
@@ -198,12 +197,11 @@ function Table({ Data, setData }) {
                 <input id="swal-input5" value=${item.latitude} class="swal2-input"><div/>
                 <div className="form-group"><label for="swal-input6" style="width: 100px;">Longitude:</label>
                 <input id="swal-input6" value=${item.longitude} class="swal2-input"><div/>`,
-
           showCancelButton: true,
           confirmButtonText: "Save",
         }).then((result) => {
           if (result.isConfirmed) {
-            //Storing the new customer details
+            // Get the new customer data
             const new_email = document.getElementById("swal-input1").value;
             const new_first_name = document.getElementById("swal-input2").value;
             const new_last_name = document.getElementById("swal-input3").value;
@@ -212,23 +210,7 @@ function Table({ Data, setData }) {
             const new_longitude = document.getElementById("swal-input6").value;
             const date = getDate();
 
-            let update = [...Data];
-            //Finding index of the customer to be updated
-            const itemIndex = update.findIndex((item) => item.id === id);
-
-            //Updating customer details
-            if (itemIndex !== -1) {
-              update[itemIndex].email = new_email;
-              update[itemIndex].first_name = new_first_name;
-              update[itemIndex].last_name = new_last_name;
-              update[itemIndex].ip = new_ip;
-              update[itemIndex].latitude = new_latitude;
-              update[itemIndex].longitude = new_longitude;
-              update[itemIndex].updated_at = date;
-              update = Sort(update);
-              setUpdatedata(update);
-              setData(update);
-            }
+            // Make an API call to update the customer
             axios
               .patch(`http://localhost:3001/api/customers${id}`, {
                 email: new_email,
@@ -239,10 +221,22 @@ function Table({ Data, setData }) {
                 longitude: new_longitude,
                 updated_at: date,
               })
+              .then((response) => {
+                if (response.status === 200) {
+                  // The customer was updated successfully, so fetch updated data
+                  axios
+                    .get("http://localhost:3001/api")
+                    .then((result) => {
+                      setData(Sort(result.data));
+                    })
+                    .catch((error) => {
+                      console.error("Error fetching data:", error);
+                    });
+                }
+              })
               .catch((err) => {
-                alert(`An error has occured while updating ${err}`);
+                alert(`An error has occurred while updating ${err}`);
               });
-            swal("Updated", "Data has been updated successfully", "success");
           }
         });
       }
@@ -280,61 +274,59 @@ function Table({ Data, setData }) {
         </div>
       </nav>
       <div className="table-responsive m-3">
-        <div className="container-fluid">
-          <div className="emp-btn">
-            <button className="btn btn-primary" onClick={() => add()}>
-              Add Employee
-            </button>
-          </div>
-          <table id="myTable" className="table table-hover table-stripped">
-            <thead className="table table-dark">
-              <tr>
-                <th>Id</th>
-                <th>Email</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>IP</th>
-                <th>Latitude</th>
-                <th>Longitude</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {records.map((item, index) => (
-                <tr className="rows" key={item.id} id={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.email}</td>
-                  <td>{item.first_name}</td>
-                  <td>{item.last_name}</td>
-                  <td>{item.ip}</td>
-                  <td>{item.latitude}</td>
-                  <td>{item.longitude}</td>
-                  <td>{item.created_at}</td>
-                  <td>{item.updated_at}</td>
-                  <td>
-                    <div className="btn-container">
-                      <button
-                        className="btn btn-danger"
-                        id={item.id}
-                        onClick={() => del(item.id)}
-                      >
-                        Delete
-                      </button>
-                      <button
-                        className="btn btn-primary"
-                        onClick={() => update(item.id)}
-                      >
-                        Edit
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div className="emp-btn">
+          <button className="btn btn-primary" onClick={() => add()}>
+            Add Employee
+          </button>
         </div>
+        <table id="myTable" className="table table-hover table-stripped">
+          <thead className="table table-dark">
+            <tr>
+              <th>Id</th>
+              <th>Email</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>IP</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((item, index) => (
+              <tr key={item.id} id={item.id}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.ip}</td>
+                <td>{item.latitude}</td>
+                <td>{item.longitude}</td>
+                <td>{item.created_at}</td>
+                <td>{item.updated_at}</td>
+                <td>
+                  <div className="btn-container">
+                    <button
+                      className="btn btn-danger"
+                      id={item.id}
+                      onClick={() => del(item.id)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => update(item.id)}
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <Page
           currentpage={currentPage}
           setcurrentpage={setcurrentPage}
